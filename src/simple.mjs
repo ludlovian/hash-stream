8ba38a54d7f294dc0502a7b247845e1a03abced7
Alan Layng <alan@readersludlow.com>
2022-11-20 14:57:45 +0000

    format

--- src/simple.mjs
+++ src/simple.mjs
@@ -1,7 +1,10 @@
 import { createReadStream } from 'fs'
 import { createHash } from 'crypto'
 
-export default async function hashFile (filename, { algo = 'md5', enc = 'hex' } = {}) {
+export default async function hashFile (
+  filename,
+  { algo = 'md5', enc = 'hex' } = {}
+) {
   const hasher = createHash(algo)
   for await (const chunk of createReadStream(filename)) {
     hasher.update(chunk)
